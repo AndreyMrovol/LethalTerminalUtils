58832b08a8469b62bb7d9341d0287038217145de
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-05-15 19:38:36 +0200

    Create Filter.cs

---
+++ TerminalUtils/Commands/Filter.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using TerminalUtils.Definitions;
+
+namespace TerminalUtils.Commands
+{
+	public class FilterCommand : TerminalCommandNode
+	{
+		public FilterCommand()
+			: base("filter")
+		{
+			RedirectToNode = CommandManager.RedirectToMoonsNode;
+		}
+
+		public override string Execute(string[] args)
+		{
+			string filterTypeName = "none";
+
+			// List<string> previewTypeNames = ["name"];
+			Dictionary<string, FilterInfoType<SelectableLevel>> infoTypes = TerminalManager
+				.FilterInfoTypes.Select(kv => kv.Value)
+				.ToDictionary(infoType => infoType.Name.ToLowerInvariant(), infoType => infoType);
+
+			Plugin.debugLogger.LogDebug($"Possible filter types: {string.Join(", ", infoTypes.Keys)}");
+
+			args.ToList()
+				.ForEach(arg =>
+				{
+					if (infoTypes.ContainsKey(arg))
+					{
+						filterTypeName = arg;
+					}
+				});
+
+			TerminalManager.CurrentFilterInfoType = infoTypes[filterTypeName];
+
+			return "";
+		}
+	}
+}
