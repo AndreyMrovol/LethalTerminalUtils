23a3197288fcdbf1c360222feb11c7d31bcce5fc
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-05-31 19:04:21 +0200

    force defaults

--- TerminalUtils/InfoTypeResolver.cs
+++ TerminalUtils/InfoTypeResolver.cs
@@ -8,11 +8,17 @@ namespace TerminalUtils
 	{
 		public static List<PreviewInfoType<SelectableLevel>> GetPreviewInfoType(string name)
 		{
-			// the default for this is "Name;Price;Weather"
+			// the default for this is "Price;Weather"
 
 			if (string.IsNullOrEmpty(name))
 			{
-				return [TerminalManager.PreviewInfoTypes["Name"]];
+				return Defaults.defaultPreviewType.Split(";").Select(typeName => TerminalManager.PreviewInfoTypes[typeName]).ToList();
+			}
+
+			if (!name.Contains("Name"))
+			{
+				name = "Name;" + name;
+				Plugin.debugLogger.LogDebug($"Preview type did not contain 'Name', defaulting to 'Name;{name}'");
 			}
 
 			string[] types = name.Split(';').Where(s => !string.IsNullOrWhiteSpace(s)).Select(s => s.Trim()).ToArray();
@@ -28,11 +34,21 @@ namespace TerminalUtils
 
 		public static FilterInfoType<SelectableLevel> GetFilterInfoType(string name)
 		{
+			if (string.IsNullOrEmpty(name))
+			{
+				return TerminalManager.FilterInfoTypes["None"];
+			}
+
 			return TerminalManager.FilterInfoTypes.FirstOrDefault(info => info.Key.ToLowerInvariant() == name.ToLowerInvariant()).Value;
 		}
 
 		public static SortInfoType<SelectableLevel> GetSortInfoType(string name)
 		{
+			if (string.IsNullOrEmpty(name))
+			{
+				return TerminalManager.SortInfoTypes["None"];
+			}
+
 			return TerminalManager.SortInfoTypes.FirstOrDefault(info => info.Key.ToLowerInvariant() == name.ToLowerInvariant()).Value;
 		}
 	}
