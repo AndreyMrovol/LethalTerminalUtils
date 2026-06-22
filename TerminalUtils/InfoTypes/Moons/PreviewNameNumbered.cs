edad96465f80ac374f738eb48279f199613f44d8
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-06-22 22:03:28 +0200

    Create PreviewNameNumbered.cs

---
+++ TerminalUtils/InfoTypes/Moons/PreviewNameNumbered.cs
@@ -0,0 +1,24 @@
+using System.Text.RegularExpressions;
+using MrovLib;
+using TerminalUtils.Definitions;
+
+namespace TerminalUtils.InfoTypes.Moons
+{
+	public class PreviewNameNumbered : PreviewInfoType<SelectableLevel>
+	{
+		public PreviewNameNumbered()
+			: base("NameNumbered")
+		{
+			this.MaxLength = LevelHelper.LongestPlanetName.Length + 4;
+		}
+
+		public override string Value(SelectableLevel inputValue)
+		{
+			string moonNumber = Regex.Match(inputValue.PlanetName, @"^\d+").Value;
+
+			{
+				return $"{moonNumber.PadLeft(3, '0')} {MrovLib.StringResolver.GetAlphanumericName(inputValue)}";
+			}
+		}
+	}
+}
