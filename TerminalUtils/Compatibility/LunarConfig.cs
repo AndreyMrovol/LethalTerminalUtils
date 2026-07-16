f4beb2e1f2a923463b2a809d847e6e30299fa794
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-07-16 23:21:21 +0200

    redid lunar config compat

--- TerminalUtils/Compatibility/LunarConfig.cs
+++ TerminalUtils/Compatibility/LunarConfig.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
-using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using Dawn;
+using HarmonyLib;
 
 namespace TerminalUtils.Compatibility
 {
@@ -9,35 +11,57 @@ namespace TerminalUtils.Compatibility
 		public LunarConfigCompatibility(string guid, string version = null)
 			: base(guid, version) { }
 
-		public Dictionary<SelectableLevel, int> newIndex = [];
+		public static Dictionary<SelectableLevel, int> newIndex = [];
 
 		public override void Init()
 		{
-			MrovLib.EventManager.ContentManagerReady.AddListener(() =>
+			Plugin.logger.LogInfo("Initializing LunarConfig compatibility...");
+
+			Plugin.harmony.Patch(
+				AccessTools.Constructor(typeof(LunarConfig.Objects.Config.LunarConfigCustomMoonOrder), [typeof(Dictionary<DawnMoonInfo, int>)]),
+				transpiler: new HarmonyMethod(AccessTools.Method(typeof(LunarConfigCompatibility), nameof(LunarConfig_GetOrderingAlgorithm)))
+			);
+		}
+
+		[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+		public static IEnumerable<CodeInstruction> LunarConfig_GetOrderingAlgorithm(IEnumerable<CodeInstruction> instructions)
+		{
+			CodeMatcher matcher = new(instructions);
+
+			var lunarCustomOrderCtor = AccessTools.Constructor(
+				typeof(LunarConfig.Objects.Config.LunarConfigCustomMoonOrder),
+				[typeof(Dictionary<DawnMoonInfo, int>)]
+			);
+
+			var passMethod = AccessTools.Method(typeof(LunarConfigCompatibility), nameof(PassNewCatalogueIndex));
+
+			// Find ctor call and insert a call right before it:
+			// stack: ... dict -> PassNewCatalogueIndex(dict) -> dict -> new LunarConfigCustomMoonOrder(dict)
+			matcher.MatchForward(false, new CodeMatch(ci => ci.opcode == OpCodes.Newobj && Equals(ci.operand, lunarCustomOrderCtor)));
+
+			if (matcher.IsValid)
 			{
-				GetMoonIndexes();
+				matcher.Insert(new CodeInstruction(OpCodes.Call, passMethod));
+			}
+
+			return matcher.InstructionEnumeration();
+		}
+
+		[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+		private static void PassNewCatalogueIndex(Dictionary<object, int> newCatalogueIndex)
+		{
+			newIndex.Clear();
+
+			MrovLib.LevelHelper.Levels.ForEach(moon =>
+			{
+				Dawn.DawnMoonInfo moonInfo = moon.GetDawnInfo();
+
+				int index = newCatalogueIndex.ContainsKey(moonInfo) ? newCatalogueIndex[moonInfo] : 0;
+				newIndex.Add(moon, index);
 			});
 		}
 
-		private void GetMoonIndexes()
-		{
-			if (LunarConfig.Objects.Config.LunarCentral.enabledMoonSettings.Contains("Catalogue Index"))
-			{
-				LunarConfig.Objects.Config.LunarConfigCustomMoonOrder ordering = (LunarConfig.Objects.Config.LunarConfigCustomMoonOrder)
-					Dawn.MoonRegistrationHandler.MoonGroupAlgorithm.OrderingSteps.First(step =>
-						step is LunarConfig.Objects.Config.LunarConfigCustomMoonOrder
-					);
-
-				MrovLib.LevelHelper.Levels.ForEach(moon =>
-				{
-					Dawn.DawnMoonInfo moonInfo = moon.GetDawnInfo();
-
-					int index = ordering.GetIndex(moonInfo);
-					newIndex.Add(moon, index);
-				});
-			}
-		}
-
+		[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
 		public int GetMoonIndex(SelectableLevel moon)
 		{
 			return newIndex.ContainsKey(moon) ? newIndex[moon] : 0;
